f9a38dfca468af5b680ec7a8daed3828bdefa243
FIC <FIC@info.local>
2019-09-13 16:03:29 -0300

    Insercação de pastas

---
+++ Aula1exercicios/ex5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            strirng nomeVendedor;
+            double salarioFixo, totalVendas;
+
+            Console.WriteLine("Digite seu nome");
+            nomeVendedor = Console.ReadLine();
+
+            salarioFixo = double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}

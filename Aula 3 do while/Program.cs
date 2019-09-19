fbe16f30dd9c55cf26d1776da04de27eb2691700
FIC <FIC@info.local>
2019-09-19 13:57:45 -0300

    Exercicio multiplos de 3

---
+++ Aula 3 do while/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula_3_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int cont = 0;
+
+            do{
+                Console.WriteLine("Digite o numero: ");
+
+                numero = int.Parse(Console.ReadLine() );
+
+                cont++;
+
+                if (numero /3 ==0)
+                {
+                    numero = numero + 1;
+
+                }
+
+            }while(cont <=20);
+               Console.WriteLine("Os multiplos de são: " + numero);
+
+
+        }
+    }
+}

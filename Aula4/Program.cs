d75ce96deddb26b99f3ea72e2026eeedfc8209ec
FIC <FIC@info.local>
2019-09-20 13:43:03 -0300

    Aula4Vetor

---
+++ Aula4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] media = new double[10];
+
+            for(int i = 0; i <= 9; i++){
+
+                Console.Write("Digite uma média: ");
+                media[i] = double.Parse(Console.ReadLine() );
+                
+            }
+
+                Console.WriteLine(media [3] );
+                Console.WriteLine();
+
+                for(int i = 0 ; i <= media.Length - 1 ; i++){ // Quando o incide for menor que media ele conta -1 ou i <media 
+
+                    Console.WriteLine("Média : " + media[1] );
+                }
+        }
+    }
+}
